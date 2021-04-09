ebe4ede80f5ffcaa030ca0360950eefbb15f62e1
SearchAndI <sninom@unal.edu.co>
2021-04-09 14:44:47 -0500

    download and unzip data

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,16 @@
 library(data.table)
 library(reshape2)
 library(dplyr)
+
+getwd()
+
+url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+compressed_file<- 'getdata_projectfiles_UCI HAR Dataset.zip'
+if(!file.exists(compressed_file)) {download.file(url,compressed_file)}
+folder <- 'UCI HAR Dataset'
+if(!file.exists(folder)) {unzip(compressed_file)}
+
+
+
+
+
