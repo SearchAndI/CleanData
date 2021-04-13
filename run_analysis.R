847d2c214a6a62d0ef7a39af6bddd4fdd26308a4
SearchAndI <sninom@unal.edu.co>
2021-04-13 17:02:35 -0500

    mean and std columns filtered

--- run_analysis.R
+++ run_analysis.R
@@ -6,6 +6,7 @@ library(dplyr)
 
 
 #Downloading and decompressing the data 
+
 url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 compressed_file<- 'getdata_projectfiles_UCI HAR Dataset.zip'
 if(!file.exists(compressed_file)) {download.file(url,compressed_file)}
@@ -14,9 +15,11 @@ if(!file.exists(folder)) {unzip(compressed_file)}
 
 
 
-# Loading into R and exploring the shape of the data files
-current_wd <- getwd() 
 
+
+# Loading into R and exploring the shape of the data files
+
+current_wd <- getwd() 
 
 train_subjects <- data.table(read.table(file.path(current_wd, folder, 'train', 'subject_train.txt')))
 test_subjects <- data.table(read.table(file.path(current_wd, folder, 'test', 'subject_test.txt')))
@@ -38,12 +41,12 @@ head(features)
 dim(features)
 feature_names <- dplyr::pull(features, V2) #used dplyr to change the class to a vector and then use the content as names for the table
 
+
+
 #The sample is of 30 people, labeled from 1 to 30; test and train subjects are different people, randomly splitted from the sample.
 #Besides, dimensionality suggests labels and subjects are column vectors associated with the same set matrix, 
 #which takes column names from  features second column vector.
 #thus, merging test/train groups by column and the resulting 2 groups by row seems reasonable.
-
-
 
 
 
@@ -52,11 +55,46 @@ feature_names <- dplyr::pull(features, V2) #used dplyr to change the class to a 
  Complete_table<- rbind(cbind(train_subjects, training_lables, training_set),
                         cbind(test_subjects,  testing_lables,  testing_set))
  
-char_feature_names <- as.vector(feature_names, mode = "character") 
+char_feature_names <- as.vector(feature_names, mode = "character") #changing the class to be able to use colnames  
 table_names <- c('subject','lable',char_feature_names) 
 
 colnames(Complete_table) <- table_names
 head(Complete_table)
+
+remove(train_subjects,training_lables,training_set,test_subjects,testing_lables,testing_set) #erasing redundant data from memory
+
+
+
+
+
+#Looking for the mean and std features
+
+feature_index <- dplyr::pull(features, V1)
+ 
+located_indexes <-  grep("mean\\(\\)|std\\(\\)", char_feature_names,perl = T)
+feature_position_index <- c( located_indexes + 2 ) # to take into account the two columns added at the beginning
+positionstofilter <- c(1,2,feature_position_index) # to actually include those 2 columns
+filtered_names <- char_feature_names[located_indexes] # just to check the regular expression was right
+
+
+
+filtered_table <- Complete_table[,..positionstofilter] #the two dots were suggested by the console,following an error
+head(filtered_table)  #that´s why i checked it was ok 
+class(filtered_table)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
